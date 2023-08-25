297cfb44639ed9bdfb5ce9a32d6f4788a6714f4c
mozamoza515 <135591897+mozamoza515@users.noreply.github.com>
2023-08-25 18:40:17 +0900

    kadai_005再提出をアップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,20 @@
+// 身長の宣言
+let height;
+
+//身長の値
+height = 1.7;
+
+// 体重の宣言
+let weight;
+
+//体重の値
+weight = 68;
+
+//BMIの宣言
+let BMI;
+
+//BMIの式
+BMI = (weight/(height*height))
+
+//BMIの値
+console.log(BMI)
