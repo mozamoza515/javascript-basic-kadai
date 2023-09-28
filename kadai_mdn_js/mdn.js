ba871ddebf97e529c4b0d9ab4d205411a9f19d22
mozamoza515 <135591897+mozamoza515@users.noreply.github.com>
2023-09-28 19:43:12 +0900

    課題mdnうぃ

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const date = new Date(2023, 8, 22);
+
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
+
+const date1 = year + '年' + month + '月' + day + '日' ;
+
+console.log(date1)
